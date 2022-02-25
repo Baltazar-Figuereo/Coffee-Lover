6ec56a90312cd9e4a198044280a6eab90d02e80e
Baltazar Figuereo <Bfiguereo40@gmail.com>
2022-02-24 19:48:34 -0500

    Added font links to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import Document, {Html, Head, NextScript, Main} from "next/document";
 class MyDocument extends Document {
     render() {
         return <Html lang="en">
-            <Head></Head>
+            <Head>
+                <link rel="preload" href="fonts/MPLUSRounded1c-Bold.ttf" as='font' crossOrigin="anonymous"></link>
+                <link rel="preload" href="fonts/MPLUSRounded1c-ExtraBold.ttf" as='font' crossOrigin="anonymous"></link>
+                <link rel="preload" href="fonts/MPLUSRounded1c-Regular.ttf" as='font' crossOrigin="anonymous"></link>
+            </Head>
             <body>
                 <Main></Main>
                 <NextScript />
