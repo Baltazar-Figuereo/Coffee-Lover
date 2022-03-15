0ed71db5e6b5a48a510a4cea3dc33ee5ec48d89e
Baltazar Figuereo <Bfiguereo40@gmail.com>
2022-03-15 19:49:47 -0400

    Added fallback and .isFallback to coffeestore

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -4,43 +4,47 @@ import Link from "next/link"
 import coffeeStoresData from "../../data/coffee-store.json";
 
 export function getStaticProps(staticProps) {
-    const params = staticProps.params;
-    // console.log("params", params)
-    return {
-        props: {
-            coffeeStore: coffeeStoresData.find(store => store.id.toString() === params.id)
-        }
-    }
+	const params = staticProps.params;
+	// console.log("params", params)
+	return {
+		props: {
+			coffeeStore: coffeeStoresData.find(store => store.id.toString() === params.id)
+		}
+	}
 }
 
 export function getStaticPaths() {
-    return {
-        paths: [
-            { params: {id: '0'} },
-            { params: {id: '1'} }
-        ],
-        fallback: false
-    }
+	return {
+		paths: [
+			{ params: {id: '0'} },
+			{ params: {id: '1'} }
+		],
+		fallback: true
+	}
 }
 
 const CoffeeStore = (props) => {
-   const router = useRouter();
-   console.log("Router", router);
-   console.log("props", props)
+	const router = useRouter();
+	console.log("Router", router);
+	console.log("props", props);
 
-   return (
-       <div>
-          Coffee Store Page {router.query.id}
-           <Link href="/">
-               <a>Back to home</a>
-           </Link>
-           <Link href="/coffee-store/dynamic">
-               <a>Back to page dynamic</a>
-           </Link>
-           <p>{props.coffeeStore.address}</p>
-           <p>{props.coffeeStore.name}</p>
-       </div>
-   );
+	if(router.isFallback) {
+		return (<div>Is Loading...</div>)
+	}
+
+	return (
+		<div>
+			Coffee Store Page {router.query.id}
+			<Link href="/">
+				<a>Back to home</a>
+			</Link>
+			<Link href="/coffee-store/dynamic">
+				<a>Back to page dynamic</a>
+			</Link>
+			<p>{props.coffeeStore.address}</p>
+			<p>{props.coffeeStore.name}</p>
+		</div>
+	);
 }
 
 export default CoffeeStore;
