d9d2626c39c7bcea20dfc95b8df1c93f3e7b65a7
Baltazar Figuereo <Bfiguereo40@gmail.com>
2022-03-15 20:05:36 -0400

    Refractored CoffeeStore dynamic page.

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,7 +1,9 @@
 import { useRouter } from "next/router";
 import Link from "next/link"
+import Head from "next/head";
 
 import coffeeStoresData from "../../data/coffee-store.json";
+
 
 export function getStaticProps(staticProps) {
 	const params = staticProps.params;
@@ -14,11 +16,16 @@ export function getStaticProps(staticProps) {
 }
 
 export function getStaticPaths() {
+	const paths = coffeeStoresData.map(coffeeStore => {
+		return {
+			params: {
+				id: coffeeStore.id.toString(),
+			}
+		}
+	})
+
 	return {
-		paths: [
-			{ params: {id: '0'} },
-			{ params: {id: '1'} }
-		],
+		paths,
 		fallback: true
 	}
 }
@@ -32,17 +39,19 @@ const CoffeeStore = (props) => {
 		return (<div>Is Loading...</div>)
 	}
 
+	const {address, name, neighbourhood} = props.coffeeStore;
+
 	return (
 		<div>
-			Coffee Store Page {router.query.id}
+			<Head>
+				<title>{name}</title>
+			</Head>
 			<Link href="/">
 				<a>Back to home</a>
 			</Link>
-			<Link href="/coffee-store/dynamic">
-				<a>Back to page dynamic</a>
-			</Link>
-			<p>{props.coffeeStore.address}</p>
-			<p>{props.coffeeStore.name}</p>
+			<p>{address}</p>
+			<p>{name}</p>
+			<p>{neighbourhood}</p>
 		</div>
 	);
 }
