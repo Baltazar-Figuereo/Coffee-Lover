28cfbf9322402024449af87d17d698601edae649
Baltazar Figuereo <Bfiguereo40@gmail.com>
2022-03-23 21:38:46 -0400

    Added Foursqures places API data to homepage.

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,27 @@ import styles from '../styles/Home.module.css'
 import Banner from "../components/banner";
 import Card from "../components/card";
 
-import coffeeStoresData from "../data/coffee-store.json";
+// import coffeeStoresData from "../data/coffee-store.json";
 
 export async function getStaticProps(context) {
     // this is all server-side code.
     console.log("Hi Static Props")
+
+	let coffeeStoresData = [];
+
+	let httpRequest = await fetch("https://api.foursquare.com/v3/places/nearby?v=20220322&query=coffee&near=New%20york%20city&radius=100000&limit=6", {
+		"headers": {
+			"Authorization": "fsq3bHgC9ocvdRV1flFjmK2MsLr78TZX1YXIQDYHQ0Xi7UQ="
+		}
+	})
+
+	coffeeStoresData = await httpRequest.json();
+
+	console.log(coffeeStoresData.results)
+
     return {
         props: {
-			coffeeStores: coffeeStoresData,
+			coffeeStores: coffeeStoresData.results,
 		}, // will be passed to the page component as props
     }
 }
@@ -50,7 +63,7 @@ export default function Home(props) {
 								<Card
 									key={coffeeStores.id}
 									name={coffeeStores.name}
-									imgUrl={coffeeStores.imgUrl}
+									imgUrl={coffeeStores.imgUrl || "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"}
 									href={`/coffee-store/${coffeeStores.id}`}
 								/>
 							);
